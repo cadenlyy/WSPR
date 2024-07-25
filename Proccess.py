49eb1d22807cbc67d44c8382fee889fd803247d2
cadenlyy <caden.lyy@gmail.com>
2024-07-25 21:56:32 +0800

    mapping transmittions

--- Proccess.py
+++ Proccess.py
@@ -1,12 +1,14 @@
 import query
 #id, time, band, rx_sign, rx_lat, rx_lon, rx_loc, tx_sign, tx_lat, tx_lon, tx_loc, distance, azimuth, rx_azimuth, frequency, power, snr, drift, version, code
-data = []
+transmitions = {}
 
 #colums, database, start dateTime, end dateTime, limit = 100
-q = query.wsprlive_get("COLUMNS('rx_sign')", "rx", '2024-07-24 09:24:00', '2024-07-24 09:25:00')
+q = query.wsprlive_get("*", "rx", '2024-07-24 09:24:00', '2024-07-24 09:25:00')
 
 for i in q:
-    data.append(list(i.values()))
+    data = list(i.values())
+    transmitions[data[3]] = []
+    transmitions[data(3)].append(data[7])
 
 print(data)
 
