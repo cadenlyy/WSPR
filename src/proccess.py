02c64e2a5017add43274a8eac8caeb8ae5311a0d
cadenlyy <caden.lyy@gmail.com>
2024-08-28 14:15:22 +0800

    added working SD calculations

--- src/proccess.py
+++ src/proccess.py
@@ -1,5 +1,6 @@
 import query
 import datetime
+import math
 
 #wspr query
 #colums, database, start dateTime, end dateTime, limit = None
@@ -13,10 +14,15 @@ print("wspr.rx query successful") #check if proccessing is slow
 #id, time, band, rx_sign, rx_lat, rx_lon, rx_loc, tx_sign, tx_lat, tx_lon, tx_loc, distance, azimuth, rx_azimuth, *frequency, power, *snr, *drift, version, code
 
 #adjList with weight
-transmitions = {} #dict mapping receiver to dict of transmitters with dictionary mapping time to 3 arrays, one of spot details, one for sum and one for mean (i.e a{{{[[[]],[],[]}}})
+transmitions = {} #dict mapping receiver to dict of transmitters with dictionary mapping time to 2 arrays, one of spot details, one for sum, mean and SD (i.e a{{{[[[]],[]}}})
 
 def roundEvenDateTime(a): #YYYY-MM-DD HH:MM:SS
     return datetime.datetime.strptime(str(a)[:15]+str(int(str(a)[15])%2+int(str(a)[15]))+str(a)[16:], '%Y-%m-%d %H:%M:%S')
+
+def SD(s, m, n):#sum of x^2, mean, number of values
+    print(s,m,n)
+    return math.sqrt(float(s/n-m**2))
+    
 
 MR = datetime.timedelta(minutes=2)
 
@@ -30,14 +36,19 @@ for i in q: #proccess each spot
         transmitions.get(tuple(data[3:7])).update({tuple(data[7:11]):{}})
     #adding new time
     if transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).get(data[1]) == None:
-        transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).update({data[1]:[[],[0,0,0],[None,None,None]]})
-    #pushing in transmittion info                                   (SD: freq, snr, drift)
+        transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).update({data[1]:[[],[0,0,0,0,0,0,None,None,None,None,None,None]]}) #[sum:freq,snr,drift,sum:freq^2,snr^2,drift^2,mean:freq,snr,drift,SD:freq,snr,drift]
+    #pushing in transmittion info                                  (SD: freq, snr, drift)
     transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).get(data[1])[0].append([0,0,0]+data)
     
     #calculating sum for freq, SNR and drift
     transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).get(data[1])[1][0]+=data[14]
     transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).get(data[1])[1][1]+=data[16]
     transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).get(data[1])[1][2]+=data[17]
+    
+    #calculating sum for freq^2, SNR^2 and drift^2
+    transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).get(data[1])[1][3]+=data[14]**2
+    transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).get(data[1])[1][4]+=data[16]**2
+    transmitions.get(tuple(data[3:7])).get(tuple(data[7:11])).get(data[1])[1][5]+=data[17]**2
 
 #sliding(only actually going through each spot once so still O(N))   
 for r in transmitions.items():
@@ -45,12 +56,15 @@ for r in transmitions.items():
         numOfSpots = 0
         left = 0
         right = 0
-        slidingWindow = [0,0,0]
+        slidingWindow = [0,0,0,0,0,0]#sum:freq,snr,drift,sum:freq^2,snr^2,drift^2
         for i in range(0, len(t[1])):
             if datetime.datetime.strptime(list(t[1])[i], '%Y-%m-%d %H:%M:%S') - ts < MR:
                 slidingWindow[0] += t[1].get(list(t[1])[i])[1][0]
                 slidingWindow[1] += t[1].get(list(t[1])[i])[1][1]
                 slidingWindow[2] += t[1].get(list(t[1])[i])[1][2]
+                slidingWindow[3] += t[1].get(list(t[1])[i])[1][3]
+                slidingWindow[4] += t[1].get(list(t[1])[i])[1][4]
+                slidingWindow[5] += t[1].get(list(t[1])[i])[1][5]
                 numOfSpots += len(t[1].get(list(t[1])[i])[0])
                 right = i+1
             elif te - datetime.datetime.strptime(list(t[1])[i], '%Y-%m-%d %H:%M:%S') < MR:
@@ -61,6 +75,9 @@ for r in transmitions.items():
                         slidingWindow[0] -= t[1].get(list(t[1])[j])[1][0]
                         slidingWindow[1] -= t[1].get(list(t[1])[j])[1][1]
                         slidingWindow[2] -= t[1].get(list(t[1])[j])[1][2]
+                        slidingWindow[3] -= t[1].get(list(t[1])[j])[1][3]
+                        slidingWindow[4] -= t[1].get(list(t[1])[j])[1][4]
+                        slidingWindow[5] -= t[1].get(list(t[1])[j])[1][5]
                         numOfSpots -= len(t[1].get(list(t[1])[j])[0])
                     else:
                         left = j
@@ -70,13 +87,22 @@ for r in transmitions.items():
                         slidingWindow[0] += t[1].get(list(t[1])[j])[1][0]
                         slidingWindow[1] += t[1].get(list(t[1])[j])[1][1]
                         slidingWindow[2] += t[1].get(list(t[1])[j])[1][2]
+                        slidingWindow[3] += t[1].get(list(t[1])[j])[1][3]
+                        slidingWindow[4] += t[1].get(list(t[1])[j])[1][4]
+                        slidingWindow[5] += t[1].get(list(t[1])[j])[1][5]
                         numOfSpots += len(t[1].get(list(t[1])[j])[0])
                     else:
                         right = j
                         break
-                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[2][0] = slidingWindow[0]/numOfSpots
-                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[2][1] = slidingWindow[1]/numOfSpots
-                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[2][2] = slidingWindow[2]/numOfSpots
+                #mean
+                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[1][6] = slidingWindow[0]/numOfSpots
+                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[1][7] = slidingWindow[1]/numOfSpots
+                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[1][8] = slidingWindow[2]/numOfSpots
+                #SD
+                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[1][9] = SD(slidingWindow[3],slidingWindow[0]/numOfSpots,numOfSpots)
+                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[1][10] = SD(slidingWindow[4],slidingWindow[1]/numOfSpots,numOfSpots)
+                transmitions.get(r[0]).get(t[0]).get(list(t[1])[i])[1][11] = SD(slidingWindow[5],slidingWindow[2]/numOfSpots,numOfSpots)
+                
                     
 print(transmitions)    
                     
