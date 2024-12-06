fa422084512c44c1cf7ce5c9e789dbb8e488d513
Loi Yi Yang Caden <99410585+cadenlyy@users.noreply.github.com>
2024-12-06 03:22:12 +0000

    checking for station with short path on qtr901 flight

--- src/main.py
+++ src/main.py
@@ -6,22 +6,75 @@ import datetime
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
+s = datetime.datetime(2022,11,1,15,0,0) #Y,M,D,h,m,s
+e = datetime.datetime(2022,11,1,23,59,0)
+#file path and naming
+base_dir = "C:\\Users\\Lyiyang1\\Desktop\\wspr\\data\\json"
+filename = 's.txt'
+abs_file = os.path.join(base_dir, filename)
+with open(abs_file, 'r') as file: 
+    data = file.read()
+tx = ''
+rx = ''
+tor = 0
+temp = 0;
+for i in range(len(data)):
+    if temp > 0:
+        temp -= 1
+        continue
+    if data[i] == '\n':
+        print(rx,tx)
+        q = query.wsprlive_get('*','rx',str(s),str(e),rx,tx)
+        print(q)
+        if len(q) == 0:
+            print(q[0])
+            
+        plt.figure(figsize=(100,60))
+        m = Basemap(projection='cyl',llcrnrlat=-90,urcrnrlat=90,llcrnrlon=-180,urcrnrlon=180,resolution='c')
+        m.drawcoastlines(linewidth=5)
+        m.drawmeridians(np.arange(0,360,30))
+        m.drawparallels(np.arange(-90,90,30))
+        plot_planes.fdrawgreatcircle([-180,180], q[0].get('rx_lon'), q[0].get('rx_lat'),  q[0].get('tx_lon'),  q[0].get('tx_lat'), m, linewidth= 10)
+        plt.title(q[0].get('rx_sign')+'_'+q[0].get('tx_sign')+'_'+str(len(q)), fontsize = 100)
+
+        
+        tx = ''
+        rx = ''
+        tor = 0
+        continue
+    elif  data[i:i+4] == ' to ':
+        tor = 1
+        temp = 3
+        continue
+    if tor:
+        rx += data[i]
+    else:
+        tx += data[i]
+    
+
+
+
+'''
 #query details
-s = datetime.datetime(2022,11,1,16,0,0) #Y,M,D,h,m,s
-e = datetime.datetime(2022,11,3,22,40,0)
+s = datetime.datetime(2022,11,1,0,0,0) #Y,M,D,h,m,s
+e = datetime.datetime(2022,11,3,0,0,0)
 qf = 'pair'
-rx = 'W3LLA'
-tx = 'WA5CB'
+rx = 'K1RA-4'
+tx = 'KJ7CQC'
 query.wspr_to_json(qf, s, e, rx, tx)
 #q = query.wsprlive_get('*','rx',s,e,rx,tx)
+'''
 
+'''
 #processing details
 MR = datetime.timedelta(minutes = 180)
 ssT = 0.5#Minimum standard score
 pf = 'r'#r(read from json), t(test case), q(query from wsprnet)
+'''
 
-
+'''
 #check details
 a = process.anomalies_freqsnr(pf,MR,ssT,s,e, rx, tx)
 #process.print_csv(qf, 'a', s, e, MR, ssT, a, rx, tx)
@@ -30,7 +83,8 @@ clat1 = -60
 clon1 = 120
 clat2 = 0
 clon2 = 180
-p = check.read_bst("1")
+#p = check.read_bst("1")
+'''
 
 
 '''
